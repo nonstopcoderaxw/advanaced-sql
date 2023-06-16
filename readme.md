d4e5fa7eb4d101f58000683f3ef22adb294757ab
axw <axw@axws-mbp.home>
2023-06-16 21:35:48 +1000

    first

--- readme.md
+++
@@ -1,4 +0,0 @@
-## export env
-		source .env
-
-## reference: https://learnsql.com/blog/25-advanced-sql-query-examples/
--- sqls/example-1/drop-db.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	DROP DATABASE IF EXISTS advanced_sql;
-EOSQL
